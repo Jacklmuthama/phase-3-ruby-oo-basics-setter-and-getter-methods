526bbc3c0588fe37928b3bfe8cdc4f04731fbb26
Jackline <jackline.muthama@student.moringaschool.com>
2023-02-09 00:15:35 +0300

    completed

--- lib/dog.rb
+++ lib/dog.rb
@@ -1,3 +1,17 @@
 class Dog
-
-end
+    def name=(value)
+      @name = value
+    end
+  
+    def name
+      @name
+    end
+  
+    def breed=(value)
+      @breed = value
+    end
+  
+    def breed
+      @breed
+    end
+  end
